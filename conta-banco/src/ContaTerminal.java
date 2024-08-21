bb0aa21983b17d4287fc90f9fb933caa561d8606
Juliana Câmara <jumedmais@gmail.com>
2024-08-21 18:05:07 -0300

    refactor: alter contaterminal.java

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nPor favor, digite o seu número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite o número da sua agência: ");
+        String numeroAgencia = scanner.nextLine();
+
+        System.out.println("Digite o seu nome: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o saldo que deseja depositar: ");
+        float saldoCliente = scanner.nextFloat();
+
+        scanner.close();
+
+        System.out.println("\nOlá, " + nomeCliente + "sua agência e seu número de conta é: " + numeroAgencia + numeroConta);
+        System.out.println("\nO seu novo saldo é de: " + saldoCliente );;
     }
 }
